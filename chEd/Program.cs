c39390da8570b51ccb5b3308e02641196ac63011
Vladi <peresvladi@nail.com>
2022-10-07 16:22:01 +0300

    shLesson3 группы методов void и группы методов исполняющие return

---
+++ chEd/Program.cs
@@ -0,0 +1,48 @@
+﻿// chLesson3
+
+
+
+/* // Method4
+
+ string Method4(int count, string text)
+ {int i = 0;
+ string result = String.Empty;
+ while(i < count)
+ {result = result + text;
+ i++;
+ }
+ return result;
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
+ */
+
+
+
+
+
+
+/* //Method3
+
+int Method3()
+{return DateTime.Now.Year;}
+int year = Method3();
+Console.WriteLine(year); */
+
+
+
+/* // Method2(Void)
+void Method2(String msg, int count)
+{int i = 0;
+while (i < count)
+{Console.WriteLine(msg);
+i++;
+}
+}
+Method2("Text", 4); */
+
+
+/* // Method1(Void)
+void Method1()
+{Console.WriteLine("Автор ...");}
+Method1(); */
