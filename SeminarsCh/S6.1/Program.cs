e308665e16685fe8de35a6915df20358666489b5
Vladi <peresvladi@nail.com>
2022-10-26 09:27:23 +0300

    функции, массивы, скопировать массив, нахождение положительных цифр в массиве, нахождение точки пересечения прямых по мат.уравнению

---
+++ SeminarsCh/S6.1/Program.cs
@@ -0,0 +1,111 @@
+﻿// ChTask45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+/* 
+
+
+Console.Clear();
+
+int[] storage = GA(9, 0, 9);
+Console.WriteLine(String.Join(" ", storage));
+
+int [] copystorage = Copystorage(storage);
+Console.WriteLine(String.Join(" ", copystorage));
+
+
+int[]GA(int size, int minValue, int maxValue)
+{
+  int[] res = new int[size];
+
+  for (int i = 0; i < size; i++)
+  {
+    res[i] = new Random().Next(minValue, maxValue + 1);
+  }
+  return res;
+}
+
+int[] Copystorage(int[]instorage)
+{
+  int[]result = new int[instorage.Length];
+  for (int i = 0; i < instorage.Length; i++)
+{
+  result[i] = instorage[i];
+
+}
+return result;
+}
+
+
+
+ */
+
+//ShTask 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+
+/* 
+
+
+Console.Clear();
+Console.Write("Введите через пробел значения b1, k1, b2, k2: ");
+string[]f = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+double b1 = double.Parse(f[0]);
+double k1 = double.Parse(f[1]);
+double b2 = double.Parse(f[2]);
+double k2 = double.Parse(f[3]);
+
+Console.WriteLine(String.Join(" ", GetPoint(b1, k1, b2, k2)));
+
+
+
+
+double[] GetPoint(double inB1, double ink1, double inB2, double ink2)
+{
+  double[] result = new double[2];
+  result[1] = (inB2 - inB1) / (ink1 - ink2);
+  result[1] = ink1 * result[0] + inB1;
+return result;
+}
+
+
+
+ */
+
+// ChTask41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+            
+
+/* 
+
+
+Console.Clear();
+
+Console.Write(" Введите числа через пробел ");
+int[]numbers = GAFS(Console.ReadLine());
+Console.WriteLine($"Колличество чисел больше 0 ->{GCPE(numbers)}");
+Console.WriteLine();
+
+
+int[]GAFS(string stringstorage)
+{
+    string[] numS = stringstorage.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+    // string[] numS = stringstorage.Split("  ", StringSplitOptions.RemoveEmptyEntries);
+    int[]result = new int[numS.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = int.Parse(numS[i]);
+    }
+    return result;
+  }
+
+int GCPE(int[] storage)
+{
+    int count = 0;
+    foreach (var item in storage)
+{
+    if(item>0) count++;
+}
+return count;
+}
+
+
+ */
---
+++ SeminarsCh/S6/Program.cs
@@ -0,0 +1,145 @@
+﻿/* Console.Clear();
+Console.WriteLine("Введите десятичное число");
+double num = double.Parse(Console.ReadLine());
+// double [] size =  new double [((long)num)];
+double size = 
+int I = 0;
+while (num <= 1)
+{
+    double DoudlNum = num / 2;
+    
+   
+   I++;
+
+
+} */
+
+
+
+
+
+/* // Вариант Моисеева К.
+
+Console.Clear();
+Console.WriteLine ("Введите стороны треугольника через пробел");
+String[]cell = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+if (FalseTrue(int.Parse(cell[0]),int.Parse(cell[1]),int.Parse(cell[2])))
+{
+Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+bool FalseTrue(int a, int b, int c)
+{
+    return ((a + b) > c && (b + c) > a && (c + a) > b);
+} */
+
+
+
+
+
+/* Console.Clear();
+int [] vol = new int [3];
+RL(vol);
+WL(vol);
+void RL(int[]arr)
+{
+    for (int i = 0; i < 3; i++)
+    {
+     Console.WriteLine($"Введите {i + 1}-ое число");
+     arr[i] = int.Parse(Console.ReadLine());
+    }
+}
+void WL(int[]array)
+{
+    if (array[0] + array[1] > array[2] && array[1] + array[2] > array[0] && array[2] + array[0] > array[1])
+{
+Console.WriteLine($"Введенные числа: {array[0]},{array[1]},{array[2]}, могут образовать  треугольник");
+} 
+else
+{
+ Console.WriteLine($"Введенные числа: {array[0]},{array[1]},{array[2]}, не могут образовать  треугольник");   
+}
+}
+ */
+
+
+
+
+/* Console.Clear();
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine());
+if (a + b > c && b + c > a && c + a > b)
+{
+Console.WriteLine("Введенные числа могут образоватьЫ  треугольник");
+} 
+else
+{
+ Console.WriteLine("Введенные числа не могут образоватm  треугольник");   
+} */
+
+
+
+
+
+
+
+
+/* // Коде перепечатан с кода Моисеева, не проверен, показывает ошибки надо разбираться
+// Перевернуть массив
+Console.Clear();
+
+int[] array = GetArray(10, 0, 10);
+Console.WriteLine(String.Join(" ",array));
+
+int[]reversArray = ReversArray2 (array);
+Console.WriteLine(String.Join(" ",reversArray));
+
+ReversArray1(array);
+Console.WriteLine(String.Join("", array));
+
+
+int [] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res - new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Pandom(),Next(minValue, maxValue + 1);
+    }
+return res;
+}
+
+void ReversArray1(int[]inArray)
+{
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+
+int k = igArray[i];
+inArray[i] = [inArray.Length - i - 1] = k;
+}
+
+   
+}
+ */
+
+
+
+/* 
+
+// Какой-то кусок кода.
+ReversArray1(array);
+Console.WriteLine(String.Join("",array));
+
+
+for (int i = 0; i < size; i++)
+{Program.cs
+    res[i] = new Random().Next(minValue, maxValue +1);
+}z */
