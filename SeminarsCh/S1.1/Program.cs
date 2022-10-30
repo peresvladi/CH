917c87c6317fa71e296a1fd1a218ac7604eca63d
Vladi <peresvladi@nail.com>
2022-10-30 23:47:58 +0300

    новые файлы SeminarsCh

---
+++ SeminarsCh/S1.1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+// int number = num;
+int sqr = number * number;
+Console.Write($"Квадрат числа (number) равняется - {sqr}. ");
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.Write($"Квадрат числа (number) равняется  - {sqr1}. ");
